1ad113b2752886d00ebeac08db34cdb8c5a58240
desaigner <sai@desaign.me>
2019-10-13 12:26:26 -0500

    new search form setup

--- new/assets/js/form.js
+++ new/assets/js/form.js
@@ -15,10 +15,10 @@ runThisDropdown = (ww, xx, yy) => {
 
 // form to create a Radio Buttons
 //n = col-md-#; xx = an array list; yy = an id to display in html; zz = name attribute
-runThisRadioBox = (n, xx, yy, zz) => {
+runThisRadioBox = (n, m, xx, yy, zz) => {
     for (let i = 0; i < xx.length; i++) {
         console.log(xx[i]);
-        let aa = $('<div></div>').addClass('input-group mb-3 col-md-'+n);
+        let aa = $('<div></div>').addClass('input-group mb-3').addClass('col-md-'+n).addClass('col-lg-'+m);
         let bb = $('<div></div>').addClass('input-group-prepend');
         let cc = $('<div></div>').addClass('input-group-text');
         let dd = $('<input type="radio">').attr('value', xx[i]).attr('aria-label', xx[i]).attr('name', zz);
@@ -35,10 +35,10 @@ runThisRadioBox = (n, xx, yy, zz) => {
 
 // form to create a checkbox
 //n = col-md-#; xx = an array list; yy = an id
-runThisCheckbox = (n, xx, yy) => {
+runThisCheckbox = (n, m, xx, yy) => {
     for (let i = 0; i < xx.length; i++) {
         console.log(xx[i]);
-        let aa = $('<div></div>').addClass('input-group mb-3 col-md-'+n);
+        let aa = $('<div></div>').addClass('input-group mb-3').addClass('col-md-'+m).addClass('col-lg-'+n);
         let bb = $('<div></div>').addClass('input-group-prepend');
         let cc = $('<div></div>').addClass('input-group-text');
         let dd = $('<input type="checkbox">').attr('value', xx[i]).attr('aria-label', xx[i]);
@@ -60,7 +60,7 @@ runThisCheckbox = (n, xx, yy) => {
 
     //MealType
     let mealtype = ['Breakfast', 'Lunch', 'Dinner', 'Snack'];
-    runThisRadioBox(3, mealtype, '.mealType', 'meal');
+    runThisRadioBox(6, 3, mealtype, '.mealType', 'meal');
 
     //DishType
     let dishtype = ['Bread', 'Cereals', 'Condiments and sauces', 'Drinks', 'Desserts', 'Main course', 'Pancake', 'Preps', 'Preserve', 'Salad', 'Sandwiches', 'Side dish', 'Soup', 'Starter', 'Sweets'];
@@ -76,6 +76,6 @@ runThisCheckbox = (n, xx, yy) => {
 
     //Anything you have in mind to choose
     let interupt = ['Alcohol Free', 'Celery Free', 'Crustcean Free', 'Dairy', 'Eggs', 'Fish', 'Gluten', 'Paleo', 'Peanuts', 'Keto', 'Kosher', 'Low Potassium', 'Lupine Free', 'Mustard Free', 'No oil added', 'No Sugar', 'Pescatarian', 'Pork Free', 'Red Meat Free', 'Sesame Free', 'Shellfish', 'Soy', 'Tree Nuts', 'Vegan', 'Vegetarian', 'Wheat Free'];
-    runThisCheckbox(4, interupt, '.specialRequest');
+    runThisCheckbox(4, 6, interupt, '.specialRequest');
 
 
